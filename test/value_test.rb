566832ff88fd3e8cd4c8217c88fe07153b03cec0
wbr <wbr@mac.com>
2017-12-07 17:25:06 -0800

    Update test labels.

--- test/value_test.rb
+++ test/value_test.rb
@@ -15,8 +15,8 @@ describe Enum::Value do
 
   describe '#enum_value' do
     describe 'sends symbol or string to Base#enum' do
-      specify { assert_equal 'right', Side.new(:right).send(:enum_value, :right) }
-      specify { assert_equal 'right', Side.new(:right).send(:enum_value, 'right') }
+      specify("with_symbol") { assert_equal 'right', Side.new(:right).send(:enum_value, :right) }
+      specify("with_string") { assert_equal 'right', Side.new(:right).send(:enum_value, 'right') }
     end
     
     describe 'sends integer to Base#index' do
@@ -53,15 +53,15 @@ describe Enum::Value do
   
   describe '#to_s' do
     describe 'gets enum_value as string' do
-      specify { assert_instance_of String, Side.new(:left).send(:to_s) }
-      specify { assert_equal 'left', Side.new(:left).send(:to_s) }
+      specify('is_string') { assert_instance_of String, Side.new(:left).send(:to_s) }
+      specify('return_correct_value') { assert_equal 'left', Side.new(:left).send(:to_s) }
     end
   end
   
   describe '#to_sym' do
     describe 'gets enum_value as symbol' do
-      specify { assert_instance_of Symbol, Side.new(:left).send(:to_sym) }
-      specify { assert_equal :left, Side.new(:left).send(:to_sym) }
+      specify('is_symbol') { assert_instance_of Symbol, Side.new(:left).send(:to_sym) }
+      specify('return_correct_value') { assert_equal :left, Side.new(:left).send(:to_sym) }
     end
     
     describe 'returns nil if enum_value does not repond_to to_sym' do
@@ -77,15 +77,15 @@ describe Enum::Value do
   
   describe '#nil?' do
     describe 'returns true if @stored_value is nil' do
-      specify { assert LoadAnyValue.new(nil).send(:nil?) }
-      specify { assert_equal false, LoadAnyValue.new(:something).send(:nil?) }
+      specify('given_nil') { assert LoadAnyValue.new(nil).send(:nil?) }
+      specify('given_non_nil') { assert_equal false, LoadAnyValue.new(:something).send(:nil?) }
     end
   end
   
   describe '#valid?' do
     describe 'returns true if @stored_value is valid enum' do
-      specify { assert LoadAnyValue.new(:left).send(:valid?) }
-      specify { assert_equal false, LoadAnyValue.new(nil).send(:valid?) }
+      specify('given_valid_value') { assert LoadAnyValue.new(:left).send(:valid?) }
+      specify('given_invalid_value') { assert_equal false, LoadAnyValue.new(nil).send(:valid?) }
     end
   end
   
@@ -113,33 +113,33 @@ describe Enum::Value do
   
   describe '#<=>' do
     describe 'comparable with symbol' do
-      specify { assert_equal -1, Side.new(:right) <=> :whole }
-      specify { assert_equal 0, Side.new(:right) <=> :right }
-      specify { assert_equal 1, Side.new(:right) <=> :left }
+      specify('less_than') { assert_equal -1, Side.new(:right) <=> :whole }
+      specify('equal_to') { assert_equal 0, Side.new(:right) <=> :right }
+      specify('greater_than') { assert_equal 1, Side.new(:right) <=> :left }
     end
     
     describe 'comparable with string' do
-      specify { assert_equal -1, Side.new(:right) <=> 'whole' }
-      specify { assert_equal 0, Side.new(:right) <=> 'right' }
-      specify { assert_equal 1, Side.new(:right) <=> 'left' }
+      specify('less_than') { assert_equal -1, Side.new(:right) <=> 'whole' }
+      specify('equal_to') { assert_equal 0, Side.new(:right) <=> 'right' }
+      specify('greater_than') { assert_equal 1, Side.new(:right) <=> 'left' }
     end
     
     describe 'comparable with integer' do
-      specify { assert_equal -1, Side.new(:right) <=> 2 }
-      specify { assert_equal 0, Side.new(:right) <=> 1 }
-      specify { assert_equal 1, Side.new(:right) <=> 0 }
+      specify('less_than') { assert_equal -1, Side.new(:right) <=> 2 }
+      specify('equal_to') { assert_equal 0, Side.new(:right) <=> 1 }
+      specify('greater_than') { assert_equal 1, Side.new(:right) <=> 0 }
     end
     
     describe 'comparable with other Value object of same enum class' do
-      specify { assert_equal -1, Side.new(:right) <=> Side.new(:whole) }
-      specify { assert_equal 0, Side.new(:right) <=> Side.new(:right) }
-      specify { assert_equal 1, Side.new(:right) <=> Side.new(:left) }
+      specify('less_than') { assert_equal -1, Side.new(:right) <=> Side.new(:whole) }
+      specify('equal_to') { assert_equal 0, Side.new(:right) <=> Side.new(:right) }
+      specify('greater_than') { assert_equal 1, Side.new(:right) <=> Side.new(:left) }
     end
     
     describe 'returns nil if uncomarable with other' do
-      specify { assert_nil Side.new(:right) <=> Object.new }
-      specify { assert_nil Side.new(:right) <=> nil }
-      specify { assert_nil Side.new(:right) <=> LoadAnyValue.new(:right) }
+      specify('with_object') { assert_nil Side.new(:right) <=> Object.new }
+      specify('with_nil') { assert_nil Side.new(:right) <=> nil }
+      specify('with_invalid_other_instance') { assert_nil Side.new(:right) <=> LoadAnyValue.new(:right) }
     end
   end
   
